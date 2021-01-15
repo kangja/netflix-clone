66c9c8110f56d7f4f01e8e442dda9c6fd82580f0
hi <kangja1996@gmail.com>
2021-01-14 21:43:02 -0500

    progress

---
+++ netflix-clone/src/Banner.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import axios from "./axios"
+import requests from "./requests"
+
+function Banner() {
+  const [movie, setMovie] = useState([]);
+  
+  useEffect{
+    () => {
+      async function fetchData() {
+        const request 
+      }
+      fetchData();
+    }, []
+  };
+  
+  return (
+    <header>
+      
+    </header>
+  )
+}
+
+export default Banner
